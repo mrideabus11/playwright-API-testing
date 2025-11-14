ae334046541fbc13527cfd6243077b32151a9d79
Pradyumna Subedi <pradyumnasubedi@gmail.com>
2025-11-14 19:02:47 +0545

    Learn first get request

--- tests/api_tests.spec.js
+++ tests/api_tests.spec.js
@@ -2,5 +2,4 @@ import { test, expect } from "@playwright/test"
 
 test("API get request", async ({ request, page }) => {
   const response = await request.get("https://reqres.in/api/users?page=2")
-  await page.pause()
 })
