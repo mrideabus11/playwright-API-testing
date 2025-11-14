8716741f7ea2e9a049c17c623fcbb624247f934b
Pradyumna Subedi <pradyumnasubedi@gmail.com>
2025-11-14 18:57:31 +0545

    First commit

---
+++ tests/api_tests.spec.js
@@ -0,0 +1,6 @@
+import { test, expect } from "@playwright/test"
+
+test("API get request", async ({ request, page }) => {
+  const response = await request.get("https://reqres.in/api/users?page=2")
+  await page.pause()
+})
