43ecb428e44e0a6de447198b51b522b641ca9470
Pradyumna Subedi <pradyumnasubedi@gmail.com>
2025-11-14 19:21:16 +0545

    Check the text in the API response

--- tests/api_tests.spec.js
+++ tests/api_tests.spec.js
@@ -3,4 +3,7 @@ import { test, expect } from "@playwright/test"
 test("API get request", async ({ request, page }) => {
   const response = await request.get("https://reqres.in/api/users?page=2")
   expect(response.status()).toBe(200)
+
+  const responseText = await response.text()
+  expect(responseText).toContain("Michael")
 })
